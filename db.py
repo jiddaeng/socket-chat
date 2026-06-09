15dad8097fe4e4c6f1fd3423df0f9cb08acf438e
jiddaeng <119052647+jiddaeng@users.noreply.github.com>
2026-06-09 22:42:24 +0900

    setup db

---
+++ db.py
@@ -0,0 +1,33 @@
+from extensions import db
+from datetime import datetime
+
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    # password = db.Column(db.String(20), unique=True, nullable=False)
+
+
+class Room(db.Model):
+    room_id = db.Column(db.Integer, primary_key=True)
+    roomname = db.Column(db.String(20), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class RoomMember(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+
+    joined_at = db.Column(db.DateTime, default=datetime.utcnow)
+    is_admin = db.Column(db.Boolean)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+
+    content = db.Column(db.Text, nullable=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
